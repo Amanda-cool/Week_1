5d105d6fc16163468101e95140c9808b54e06904
Amanda Davis <amdavis@redventures.com>
2020-11-05 15:58:26 -0500

    SQL Practice, Figma current update

--- ITA_Phase_2_ADavis/Week_1/database/practice.sql
+++ ITA_Phase_2_ADavis/Week_1/database/practice.sql
@@ -13,7 +13,7 @@ SELECT
 FirstName,
 LastName,
 HireDate
-FROM Employees;
+FROM Employees
 WHERE 
 Title = 'Sales Representative';
 
@@ -22,14 +22,69 @@ SELECT
 FirstName,
 LastName,
 HireDate
-FROM Employees;
+FROM Employees
 WHERE 
 Title = 'Sales Representative'
-AND Country = 'United States'
+AND Country = 'United States';
 
 -- Problem 5
 
 SELECT
 OrderID,
 OrderDate
-FROM Orders O LEFT JOIN Employees E ON O.EmployeeID = E.EmployeeID
+FROM Orders
+WHERE
+EmployeeID = 5;
+
+-- Problem 6
+
+SELECT
+SupplierID,
+ContactName,
+ContactTitle
+FROM Suppliers
+WHERE NOT 'Marketing Manager';
+
+-- Problem 7
+
+SELECT
+ProductID,
+ProductName
+FROM Products
+WHERE 
+ProductName LIKE '%queso%';
+
+-- Problem 8
+
+SELECT
+OrderID,
+CustomerID,
+ShipCountry
+FROM Orders
+WHERE 
+ShipCountry = 'France' OR "Belgium";
+
+-- Problem 9
+
+SELECT
+OrderID,
+CustomerID,
+ShipCountry
+FROM Orders
+WHERE 
+ShipCountry in (
+    'Brazil',
+    'Mexico',
+    'Aregentina',
+    'Venezuela'
+);
+
+-- Problem 10
+
+SELECT 
+FirstName,
+LastName,
+Title,
+BirthDate
+FROM Employees
+ORDER BY BirthDate DESC;
